1ec910c40bef61c56832eb10c3b9b12c4a8b4eb5
Tamas Neltz <tamas.neltz@gmail.com>
2018-06-17 15:27:01 -0400

    add tests

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,6 +5,8 @@ object Dependencies {
   val defaultDependencies = Seq(
     "org.typelevel" %% "cats-core" % catsVersion,
     "org.typelevel" %% "cats-kernel" % catsVersion,
-    "org.typelevel" %% "cats-macros" % catsVersion
+    "org.typelevel" %% "cats-macros" % catsVersion,
+    "org.scalactic" %% "scalactic" % "3.0.5",
+    "org.scalatest" %% "scalatest" % "3.0.5" % "test"
   )
 }
